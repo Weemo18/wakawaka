a81337637aa3c093db0d37823b1b582f6fb531f0
Anna <annalisa.cammisa@gmail.com>
2022-03-29 22:09:08 +0100

    making map level

---
+++ js/app.js
@@ -0,0 +1,48 @@
+var grid = []
+grid.push(['a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', '6', '7', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c'])
+grid.push(['d', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', '2'])
+grid.push(['d', 'e', 'f', 'g', 'g', 'h', 'e', 'f', 'g', 'g', 'g', 'h', 'e', 'j', 'j', 'e', 'f', 'g', 'g', 'h', 'e', 'f', 'g', 'g', 'g', 'h', 'e', '2'])
+grid.push(['d', 'i', 'j', 'o', 'o', 'j', 'e', 'j', 'o', 'o', 'o', 'j', 'e', 'j', 'j', 'i', 'j', 'o', 'o', 'j', 'e', 'j', 'o', 'o', 'o', 'j', 'e', '2'])
+grid.push(['d', 'e', 'k', 'g', 'g', 'l', 'e', 'k', 'g', 'g', 'g', 'l', 'e', 'k', 'l', 'e', 'k', 'g', 'g', 'l', 'e', 'k', 'g', 'g', 'g', 'l', 'e', '2'])
+grid.push(['d', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', '2'])
+grid.push(['d', 'e', 'f', 'g', 'g', 'h', 'e', 'f', 'h', 'e', 'f', 'g', 'g', 'g', 'g', 'g', 'g', 'h', 'e', 'f', 'g', 'g', 'h', 'e', 'f', 'h', 'e', '2'])
+grid.push(['d', 'e', 'k', 'g', 'g', 'l', 'e', 'j', 'j', 'e', 'k', 'g', 'g', 'h', 'f', 'g', 'g', 'l', 'e', 'j', 'j', 'e', 'k', 'g', 'g', 'l', 'e', '2'])
+grid.push(['d', 'e', 'e', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'e', 'e', '2'])
+grid.push(['m', '5', '5', '5', '5', 'c', 'e', 'j', 'k', 'g', 'g', 'h', 'o', 'j', 'j', 'o', 'f', 'g', 'g', 'l', 'j', 'e', 'a', '5', '5', '5', '5', '2'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'd', 'e', 'j', 'f', 'g', 'g', 'l', 'o', 'k', 'l', 'o', 'k', 'g', 'g', 'h', 'j', 'e', '2', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'd', 'e', 'j', 'j', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'j', 'j', 'e', '2', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'd', 'e', 'j', 'j', 'o', 'p', '3', '3', 'r', 'r', '3', '3', 's', 'o', 'j', 'j', 'e', '2', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['b', 'b', 'b', 'b', 'b', 'n', 'e', 'k', 'l', 'o', 't', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'o', 'k', 'l', 'e', 'm', 'b', 'b', 'b', 'b', 'b'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'o', 'e', 'o', 'o', 'o', 't', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'o', 'o', 'o', 'e', 'o', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['5', '5', '5', '5', '5', 'c', 'e', 'f', 'h', 'o', 't', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'o', 'f', 'h', 'e', 'a', '5', '5', '5', '5', '5'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'd', 'e', 'j', 'j', 'o', 'v', '4', '4', '4', '4', '4', '4', 'w', 'o', 'j', 'j', 'e', '2', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'd', 'e', 'j', 'j', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'j', 'j', 'e', '2', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['o', 'o', 'o', 'o', 'o', 'd', 'e', 'j', 'j', 'o', 'f', 'g', 'g', 'g', 'g', 'g', 'g', 'h', 'o', 'j', 'j', 'e', '2', 'o', 'o', 'o', 'o', 'o'])
+grid.push(['a', 'b', 'b', 'b', 'b', 'n', 'e', 'k', 'l', 'o', 'k', 'g', 'g', 'h', 'f', 'g', 'g', 'l', 'o', 'k', 'l', 'e', 'm', 'b', 'b', 'b', 'b', 'c'])
+grid.push(['d', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', '2'])
+grid.push(['d', 'e', 'f', 'g', 'g', 'h', 'e', 'f', 'g', 'g', 'g', 'h', 'e', 'j', 'j', 'e', 'f', 'g', 'g', 'g', 'h', 'e', 'f', 'g', 'g', 'h', 'e', '2'])
+grid.push(['d', 'e', 'k', 'g', 'h', 'j', 'e', 'k', 'g', 'g', 'g', 'l', 'e', 'k', 'l', 'e', 'k', 'g', 'g', 'g', 'l', 'e', 'j', 'f', 'g', 'l', 'e', '2'])
+grid.push(['d', 'i', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'o', 'o', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'i', '2'])
+grid.push(['x', 'g', 'h', 'e', 'j', 'j', 'e', 'f', 'h', 'e', 'f', 'g', 'g', 'g', 'g', 'g', 'g', 'h', 'e', 'f', 'h', 'e', 'j', 'j', 'e', 'f', 'g', 'y'])
+grid.push(['z', 'g', 'l', 'e', 'k', 'l', 'e', 'j', 'j', 'e', 'k', 'g', 'g', 'h', 'f', 'g', 'g', 'l', 'e', 'j', 'j', 'e', 'k', 'l', 'e', 'k', 'g', '1'])
+grid.push(['d', 'e', 'e', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'j', 'j', 'e', 'e', 'e', 'e', 'e', 'e', '2'])
+grid.push(['d', 'e', 'f', 'g', 'g', 'g', 'g', 'l', 'k', 'g', 'g', 'h', 'e', 'j', 'j', 'e', 'f', 'g', 'g', 'l', 'k', 'g', 'g', 'g', 'g', 'h', 'e', '2'])
+grid.push(['d', 'e', 'k', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'l', 'e', 'k', 'l', 'e', 'k', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'l', 'e', '2'])
+grid.push(['d', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', '2'])
+grid.push(['m', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', '5', 'n'])
+
+
+grid.forEach(function (row) {
+    var rowElem = document.createElement('div');
+    rowElem.classList.add('row')
+    row.forEach(function (col) {
+        colElem = document.createElement('div');
+        colElem.classList.add('col')
+        colElem.classList.add('col-' + col)
+        rowElem.append(colElem)
+    });
+    document.body.appendChild(rowElem);
+})
+
+
+
